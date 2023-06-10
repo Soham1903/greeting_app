2c3c8565e77a11d37ca59315e4752af77b70ade2
soham <sohamladgaonkar19@gmail.com>
2023-06-10 12:07:47 +0530

    https://github.com/Soham1903/React-Greeting-App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+let date = new Date(2020, 5, 2, 20);
+let time = date.getHours();
+let greeting = "";
+
+const cssStyle = {};
+
+if (time >= 1 && time < 12) {
+  greeting = "Good Morning!";
+  cssStyle.color = "green";
+} else if (time >= 12 && time < 19) {
+  greeting = "Good Afternoon!";
+  cssStyle.color = "Orange";
+} else {
+  greeting = "Good Night!";
+  cssStyle.color = "Black";
+}
+ReactDOM.render(
+  <div>
+    <h1>
+      Hello Sir, <span style={cssStyle}>{greeting}</span>
+    </h1>
+  </div>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
